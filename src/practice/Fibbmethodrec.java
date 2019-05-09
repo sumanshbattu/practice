ea89fbdd880b1e82daaaae569fd69a36b7700d7f
sumanshbattu <suamashabattu@gmail.com>
2019-05-09 19:59:43 +0530

    changed a bit

--- src/practice/Fibbmethodrec.java
+++ src/practice/Fibbmethodrec.java
@@ -8,7 +8,7 @@ public class Fibbmethodrec {
 		int number=new Scanner(System.in).nextInt();
 		for(int i=1;i<=number;i++)
 		{
-			System.out.print(fabb(i)+" ");
+			System.out.print(fabb2(i)+" ");
 		}
 		}
 	public static int fabb(int number)
@@ -25,8 +25,23 @@ public class Fibbmethodrec {
 		}
 		return number;
 		
+		}
+	
+	public static int fabb2(int number)
+	{
+		if (number==1||number==2)
+		{
+			return 1;
+			}
+		int a=1,b=1,c=1;
+		for (int i=3;i<=number;i++)
+		{
 		
+		c=a+b;
+	     a=b;
+		b=c;
+		}
 		
-	}
-
-}
+		return c;
+		
+	}}
